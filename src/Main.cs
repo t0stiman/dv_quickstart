507408fdd8938555622a8845bc093e027535c59e
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-11-08 18:00:56 +0100

    initial commit

---
+++ src/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace dv_quickstart;
+
+internal static class Main
+{
+	private static UnityModManager.ModEntry MyModEntry;
+	private static Harmony harmony;
+
+	//================================================================
+
+	private static bool Load(UnityModManager.ModEntry modEntry)
+	{
+		try
+		{
+			MyModEntry = modEntry;
+			
+			MyModEntry.OnUnload = OnUnload;
+
+			harmony = new Harmony(MyModEntry.Info.Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+		catch (Exception ex)
+		{
+			MyModEntry.Logger.LogException($"Failed to load {MyModEntry.Info.DisplayName}:", ex);
+			harmony?.UnpatchAll(MyModEntry.Info.Id);
+			return false;
+		}
+		
+		MyModEntry.Logger.Log("loaded");
+
+		return true;
+	}
+
+	private static bool OnUnload(UnityModManager.ModEntry modEntry)
+	{
+		harmony?.UnpatchAll(MyModEntry.Info.Id);
+		return true;
+	}
+
+	// Logger Commands
+	public static void Log(string message)
+	{
+		MyModEntry.Logger.Log(message);
+	}
+
+	public static void Warning(string message)
+	{
+		MyModEntry.Logger.Warning(message);
+	}
+
+	public static void Error(string message)
+	{
+		MyModEntry.Logger.Error(message);
+	}
+}
---
+++ src/MainMenu_Patch.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using DV.UI;
+using DV.UserManagement;
+using DV.Utils;
+using HarmonyLib;
+
+namespace dv_quickstart;
+
+[HarmonyPatch(typeof(MainMenu))]
+[HarmonyPatch(nameof(MainMenu.Start))]
+public class MainMenu_Patch
+{
+	private static void Postfix(MainMenu __instance)
+	{
+		var args = System.Environment.GetCommandLineArgs();
+		
+		//find quickstart argument
+		if (args.All(argument => argument != "-quickstart")) return;
+		
+		var gameSession = SingletonBehaviour<UserManager>.Instance.CurrentUser.CurrentSession;
+		if (gameSession == null)
+		{
+			Main.Error("There are no sessions");
+			return;
+		}
+
+		if (gameSession.LatestSave == null)
+		{
+			Main.Error("Session " + gameSession.Name + " doesn't have any saves");
+			return;
+		}
+			
+		__instance.OnContinueGameRequested(gameSession.LatestSave);
+	}
+}
